c5b3469712a9a9af88db8c98654066d0d3b0cc8f
amitr3647 <amitr3647@gmail.com>
2021-09-27 09:24:42 +0530

    learn

---
+++ app.js
@@ -0,0 +1,14 @@
+// var a=10;
+// const b=function(){
+//     const x=10;
+// }
+// // console.log(a);
+// console.log(window.a)
+// console.log(this.a)
+console.log("it works");
+
+for (var i = 0; i++; i < 5) {
+  setTimeout(function () {
+    console.log("amit");
+  }, 1000);
+}
